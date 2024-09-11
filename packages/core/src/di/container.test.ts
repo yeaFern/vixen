3ec35f41fe18128eb144d4b4612971b0fc41dabc
yeaFern <54524320+yeaFern@users.noreply.github.com>
2024-09-11 18:08:45 +1000

    initial commit

---
+++ packages/core/src/di/container.test.ts
@@ -0,0 +1,6 @@
+describe('Container', () => {
+    // ...
+    it('is a placeholder test', () => {
+        expect(0).toEqual(0);
+    });
+});
---
+++ packages/core/src/di/container.ts
@@ -0,0 +1,5 @@
+/**
+ * Implements an inversion-of-control container which can be used to instantiate
+ * objects and their dependencies.
+ */
+export class Container {}
